53276de3894873dae5c87bf30be0979fb73de0a5
Kuldeep Kumar <118204485+Kuldeep-Angural@users.noreply.github.com>
2024-10-04 12:00:22 +0530

    Update ProjectsCard.js

--- src/pages/projects/ProjectsCard.js
+++ src/pages/projects/ProjectsCard.js
@@ -1,10 +1,10 @@
-import { Box, Button, CardMedia, Grid, Link, Typography } from '@mui/material'
-import React, { useEffect, useState } from 'react'
+import { Box, Button, CardMedia, Grid, Link, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { projects } from '../../constants/projects';
-export const ProjectsCard = (datprojetcs) => {
 
+export const ProjectsCard = (datprojetcs) => {
     const [data, setData] = useState([]);
     const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -21,21 +21,19 @@ export const ProjectsCard = (datprojetcs) => {
     };
 
     useEffect(() => {
-
         setData(projects);
-    }, [])
-
+    }, []);
 
     return (
-        <Grid container >
-            <Grid item md={12}>
+        <Grid container>
+            <Grid item md={12} sx={{ position: 'relative' }}>
                 <CardMedia
                     component="img"
                     height="600px"
                     src={data?.[currentIndex]?.image}
                     sx={{ width: '100%', backgroundPosition: 'cover', border: '1px solid black' }}
                 />
-                <Box sx={{ position: 'absolute', top: '160px', height: '600px', width: '100%', backgroundColor: 'black', opacity: '0.6', }}>
+                <Box sx={{ position: 'absolute', top: '0px', height: '100%', width: '100%', backgroundColor: 'black', opacity: '0.6' }}>
                     <Typography color={'white'} fontWeight={600} position={'relative'} ml={2} mt={1}>
                         {data?.[currentIndex]?.name}
                     </Typography>
@@ -45,34 +43,26 @@ export const ProjectsCard = (datprojetcs) => {
                     </Typography>
 
                     <Typography color={'white'} fontWeight={600} position={'relative'} ml={2} mt={2}>
-                        Technologires used to Built {data?.[currentIndex]?.webLink && <Link ml={3} href={data?.[currentIndex]?.webLink}> Open</Link>}
+                        Technologies used to Build {data?.[currentIndex]?.webLink && <Link ml={3} href={data?.[currentIndex]?.webLink}> Open</Link>}
                     </Typography>
                     <Box display={'flex'} justifyContent={'flex-start'}>
-                        {data?.[currentIndex]?.techStack?.map((item) => {
-                            return (
-                                <Typography color={'white'} fontWeight={600} position={'relative'} ml={2} mt={0}>
-                                    {item}
-                                </Typography>
-                            )
-                        })}
+                        {data?.[currentIndex]?.techStack?.map((item, index) => (
+                            <Typography key={index} color={'white'} fontWeight={600} position={'relative'} ml={2} mt={0}>
+                                {item}
+                            </Typography>
+                        ))}
                     </Box>
                 </Box>
 
-
-                <Box position={'absolute'} sx={{ height: '150px', width: '250px', backgroundColor: 'transparent', bottom: '60px', right: '40px' }}>
-                    <Button onClick={previousClick} variant='text' sx={{ position: 'relative', top: '65%', left: '1px', color: 'blue', }}><KeyboardArrowLeftIcon color='pink' /></Button>
-                    <Button onClick={nextClick} sx={{ position: 'relative', top: '65%', right: '-120px', color: 'blue', }}><KeyboardArrowRightIcon /></Button>
-
-                    <CardMedia
-                        component="img"
-                        height="100%"
-                        src={data?.[currentIndex]?.image}
-                        sx={{ width: '100%', backgroundPosition: 'cover', border: '1px solid black' }}
-                    />
-
+                <Box  display="flex"  justifyContent="space-between"  alignItems="center"  position="absolute"  top="50%"  left="0"  right="0" sx={{ transform: 'translateY(-50%)', zIndex: 1 }}>
+                    <Button onClick={previousClick} variant='text'sx={{ color: 'white',height:'120px', maxWidth:'20px',backgroundColor:'grey' ,opacity: '0.4' ,margin:'6px'}}>
+                        <KeyboardArrowLeftIcon />
+                    </Button>
+                    <Button onClick={nextClick} variant='text' sx={{ color: 'white',height:'120px', maxWidth:'20px',backgroundColor:'grey' ,opacity: '0.4' , margin:'6px'}}>
+                        <KeyboardArrowRightIcon  />
+                    </Button>
                 </Box>
             </Grid>
         </Grid>
-    )
-}
-
+    );
+};
