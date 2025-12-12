d52c609bb7d89824a1ca778d545ddda6a022fb13
kuldeep <kuldeep.navv@gmail.com>
2025-12-12 15:58:58 +0530

    minor fixing

--- src/app/App.js
+++ src/app/App.js
@@ -1,30 +1,26 @@
-import { CircularProgress, createTheme, CssBaseline, Paper, Stack, ThemeProvider, Typography, } from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider
+} from "@mui/material";
+import { useState } from "react";
 import { Wrapper } from "../component/wrapper/Wrapper";
-import { useEffect, useState } from "react";
-import { addDelay, isDarkMode } from "../util/util";
+import { isDarkMode } from "../util/util";
 
 function App() {
-
   const [darkMode, setDarkMode] = useState(isDarkMode());
-  const [loading, setLoading] = useState(true);
-
   const theme = createTheme({
     palette: {
-      primary: { main: '#9E9999' },
-      secondary: { main: '#000000' },
-      mode: darkMode ? 'dark' : 'light',
+      primary: { main: "#9E9999" },
+      secondary: { main: "#000000" },
+      mode: darkMode ? "dark" : "light",
     },
   });
 
- 
-
-
   return (
-   
-      <ThemeProvider theme={theme}>
-        <Wrapper/>
-      </ThemeProvider>
-    )
+    <ThemeProvider theme={theme}>
+      <Wrapper />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/component/wrapper/Wrapper.js
+++ src/component/wrapper/Wrapper.js
@@ -1,5 +1,11 @@
-import { Box, CircularProgress, createTheme, CssBaseline, Paper, Stack, ThemeProvider } from "@mui/material";
-import { useEffect, useState } from "react";
+import {
+    Box,
+    createTheme,
+    CssBaseline,
+    Paper,
+    ThemeProvider
+} from "@mui/material";
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Contact from "../../pages/about/About";
 import Blogs from "../../pages/blogs/Blogs";
@@ -8,65 +14,47 @@ import HomePage from "../../pages/home/HomePage";
 import { PageNotFound } from "../../pages/notFound/PageNotFound";
 import Projects from "../../pages/projects/Projects";
 import Skills from "../../pages/skills/Skills";
-import { addDelay, isDarkMode } from "../../util/util";
+import { isDarkMode } from "../../util/util";
 import NavBar from "../navbar/NavBar";
 
 export const Wrapper = (props) => {
+  const ROUTES = [
+    { name: "/", value: (index) => <HomePage key={index} /> },
+    { name: "/Journey", value: (index) => <Journey key={index} /> },
+    { name: "/projects", value: (index) => <Projects key={index} /> },
+    { name: "/skills", value: (index) => <Skills key={index} /> },
+    { name: "/blogs", value: (index) => <Blogs key={index} /> },
+    { name: "/contact", value: (index) => <Contact key={index} /> },
+  ];
 
-    const ROUTES = [
-        { name: '/', value: (index) => (<HomePage key={index}   />) },
-        { name: '/Journey', value: (index) => (<Journey key={index}   />) },
-        { name: '/projects', value: (index) => <Projects key={index}   /> },
-        { name: '/skills', value: (index) => <Skills key={index}   /> },
-        { name: '/blogs', value: (index) => (<Blogs key={index}   />) },
-        { name: '/contact', value: (index) => (<Contact key={index}   />) },
-    ];
+  const [darkMode, setDarkMode] = useState(isDarkMode());
+  const [loading, setLoading] = useState(true);
 
-    const [darkMode, setDarkMode] = useState(isDarkMode());
-    const [loading, setLoading] = useState(true);
+  const theme = createTheme({
+    palette: {
+      primary: { main: "#9E9999" },
+      secondary: { main: "#000000" },
+      mode: darkMode ? "dark" : "light",
+    },
+  });
 
-    const theme = createTheme({
-        palette: {
-            primary: { main: '#9E9999' },
-            secondary: { main: '#000000' },
-            mode: darkMode ? 'dark' : 'light',
-        },
-    });
-
-    useEffect(() => {
-        let randomNumber = Math.random() + 3000;
-        addDelay(randomNumber).then(() => {
-            setLoading(false);
-        });
-    }, []);
-
-    return (
-
-        loading === true ? (
-            <Stack spacing={2} sx={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', }} direction="column" >
-                <CircularProgress color="inherit" thickness={10} size={100} />
-            </Stack>
-        ) : (
-
-            <>
-                <ThemeProvider theme={theme}>
-                    <CssBaseline />
-                    <Paper>
-                    </Paper>
-                    <Box sx={{ position: 'sticky', top: 0, zIndex: 1000 }}>
-                        <NavBar chacked={darkMode} onChange={() => setDarkMode(!darkMode)} />
-                    </Box>
-                    <Box mt={4}>
-                        <Routes>
-                            {ROUTES.map((route, index) => (
-                                <Route path={route.name} element={route.value(index) }/>
-                            ))}
-                            <Route path="*" element={<PageNotFound />} />
-                        </Routes>
-                    </Box>
-                </ThemeProvider>
-            </>
-        ))
-
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Paper></Paper>
+        <Box sx={{ position: "sticky", top: 0, zIndex: 1000 }}>
+          <NavBar chacked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+        </Box>
+        <Box mt={4}>
+          <Routes>
+            {ROUTES.map((route, index) => (
+              <Route path={route.name} element={route.value(index)} />
+            ))}
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Box>
+      </ThemeProvider>
+    </>
+  );
 };
-
